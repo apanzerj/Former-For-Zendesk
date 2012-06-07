7ee6bbeb0852d30de22d709ad7117a70c4170aad
Adam Panzer <apanzerj@gmail.com>
2012-06-07 09:23:15 -0700

    Added basic files!
    This is just an add for the basic project files.

---
+++ former.php
@@ -0,0 +1,42 @@
+<?php
+define("ZDAPIKEY", "");
+define("ZDUSER", "");
+define("ZDURL", "");
+
+function curlWrap($url, $json, $action)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 10 );
+	curl_setopt($ch, CURLOPT_URL, ZDURL.$url);
+	curl_setopt($ch, CURLOPT_USERPWD, ZDUSER."/token:".ZDAPIKEY);
+	switch($action){
+		case "POST":
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+			break;
+		case "GET":
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			break;
+		default:
+			break;
+	}
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+	curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	$decoded = json_decode($output);
+	return $decoded;
+}
+foreach($_POST as $key => $value){
+	if(preg_match('/^z_/i',$key)){
+		$arr[strip_tags($key)] = strip_tags($value);
+	}
+}
+$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'description' => $arr['z_description'], 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))), JSON_FORCE_OBJECT);
+$return = curlWrap("/tickets.json", $create, "POST");
+?>
+	
+	
