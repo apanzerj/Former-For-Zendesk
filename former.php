e4a44d2ed5d206bcb31d4deb15fe119d58274d07
apanzerj <apanzer@zendesk.com>
2012-09-04 15:53:57 -0700

    updating code
    custom field support (yeah!)

--- former.php
+++ former.php
@@ -2,31 +2,25 @@
 include("config.php");
 
 /* Notes
+ *
  * This script expects there to be a config.php file in the same directory as this file.
  * There is an included config.example which you need to edit with your own values.
  * Remember: ZDURL needs to be of the form https://subdomain.zendesk.com/api/v2 with no trailing slash
+ *
+ * Do not implement this yet. It has bugs!!! 
  */
 
-function curlWrap($url, $json, $action)
+function curlWrap($url, $json)
 {
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
 	curl_setopt($ch, CURLOPT_MAXREDIRS, 10 );
 	curl_setopt($ch, CURLOPT_URL, ZDURL.$url);
 	curl_setopt($ch, CURLOPT_USERPWD, ZDUSER."/token:".ZDAPIKEY);
-	switch($action){
-		case "POST":
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-			break;
-		case "GET":
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-			break;
-		default:
-			break;
-	}
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
 	curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 	$output = curl_exec($ch);
@@ -39,6 +33,20 @@ foreach($_POST as $key => $value){
 		$arr[strip_tags($key)] = strip_tags($value);
 	}
 }
-$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'description' => $arr['z_description'], 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))), JSON_FORCE_OBJECT);
-$return = curlWrap("/tickets.json", $create, "POST");
+$ticket = array('ticket' => array('subject' => $arr['z_subject'], 'description' => $arr['z_description'], 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester'])));
+if(CUSTOM){
+	foreach($_POST as $key => $value){
+		if(preg_match('/^c_/i',$key)){
+			$id = str_replace('c_', '', strip_tags($key));
+			$value = strip_tags($value);
+			$cfield=array('id'=>$id, 'value'=>$value);
+			$ticket['ticket']['fields'][]=$cfield;
+		}
+	}
+	$ticket = json_encode($ticket);
+}else{
+	$ticket = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'description' => $arr['z_description'], 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))));	
+}
+$return = curlWrap("/tickets.json", $ticket);
+print_r($ticket);
 ?>
