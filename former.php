a370c6c7da18d57fd8492c6bbe316aa78312ec37
Adam Panzer <apanzerj@gmail.com>
2012-10-08 10:52:05 -0700

    Update former.php
    Had to accomodate the change to the API.

--- former.php
+++ former.php
@@ -39,6 +39,6 @@ foreach($_POST as $key => $value){
 		$arr[strip_tags($key)] = strip_tags($value);
 	}
 }
-$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'description' => $arr['z_description'], 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))), JSON_FORCE_OBJECT);
+$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'comment' => array( "value"=> $arr['z_description']), 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))));
 $return = curlWrap("/tickets.json", $create, "POST");
 ?>
